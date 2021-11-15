3fb6da5bf8a79878ddcdc0353d4d3c7f0ab684bb
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-11-14 19:21:57 -0600

    View all users and charity/donations

--- app.py
+++ app.py
@@ -3,13 +3,56 @@
 # If you ever want to deactivate your virtual environment, just type deactivate in your terminal window.#
 # export FLASK_ENV=development; flask run
 # http://127.0.0.1:5000/
+# localhost:5000
 
 from flask import Flask, render_template
 
+#create instance of the flask server (Server:Flask)
+#as the root directory within 'main.py'
 app = Flask(__name__)
 
+# create some routes!
+# Anytime I want to interact with the SERVER FLASK I have to reference the "app" variable
+# the @ is a python DECORATOR
+# The / is the DEFAULT slash
+""" 
+/users	        GET	        index	    See all users
+/users/new	    GET	        new	      See new user form
+/users	        POST	      create	  Create a new user
+/users/:id	    GET	        show	    See one user
+/users/:id/edit	GET	        edit	    See an edit user form
+/users/:id	    PATCH/PUT	  update	  Update a user
+/users/:id	    DELETE	    destroy	  Delete a user
+
+/donations	        GET	        index	    See all donations
+/donations/new	    GET	        new	      See new donation form
+/donations	        POST	      create	  Create a new donation
+/donations/:id	    GET	        show	    See one donation
+/donations/:id/edit	GET	        edit	    See an edit donation form
+/donations/:id	    PATCH/PUT	  update	  Update a donation
+/donations/:id	    DELETE	    destroy	  Delete a donation
+
+/charities	        GET	        index	    See all charities
+/charities/new	    GET	        new	      See new charity form
+/charities	        POST	      create	  Create a new charity
+/charities/:id	    GET	        show	    See one charity
+/charities/:id/edit	GET	        edit	    See an edit charity form
+/charities/:id	    PATCH/PUT	  update	  Update a charity
+/charities/:id	    DELETE	    destroy	  Delete a charity
+ """
+
+users = [
+    { 'name': 'Jim Corning', 'charity': 'Humane Society', 'donation': 1000 },
+    { 'name': 'George Lucas', 'charity': 'Dogs For Life', 'donation': 250 }
+]
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
+def users_index():
+  #Show All users (and their total donations)
+  return render_template('users_index.html', users=users)
+
+# turn the server on for serving... servering!
+if __name__ == '__main__':
+  app.run(debug=True, port=3000)
+
+                 
