8b2e11d71d8225667418255fe71ab549ef6d5074
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-11-20 11:01:04 -0600

    Added Charity & Donation. Updated Style

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Charity-Contractor-Project
+Welcome to your brand new dashboard for your Donation Tracker! This is an easy way to track the amount donated to charity every year by your individual clients. Currently, new clients and new donations can be added with a quick summary showing total donations for the year. Inside each client file is a breakdown of the charities donated to, and total for each charity as well. 
+
+## How To Use:
+Either visit your website, or in your terminal run:
+export FLASK_ENV=development; flask run
+http://127.0.0.1:5000/
+
+### Actions:
+- View All Users in a Dashboard
+- Create User Accounts
+- Edit/Update User Accounts
+- Delete User Accounts
+- Create Donations
+- Edit/Update Donations
+- Delete Donations
+
+## Programming Languages Used:
+- Python
+- Flask
+- MongoDB
+
+Built for the [Contractor Project](https://docs.google.com/document/d/1bjRtmABlX9Vcl3SNQ9_WZ9eggwOsETaw1efCFdg2uW8/edit) in our ACS 1710 class
--- app.py
+++ app.py
@@ -6,59 +6,21 @@
 # http://127.0.0.1:5000/
 # localhost:5000
 
-# MongoDB 
-# brew services start mongodb-community@5.0 
-# Verify that it's running with : brew services list
+# MongoDB     brew services start mongodb-community@5.0     Verify that it's running with : brew services list
 # to stop MongoDB brew services stop mongodb-community@5.0
-# 
 
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-"""
-import os
-# pip install dnspython
-# pip3 install certifi
-import certifi
-# Info from wd3.myworkday.com/shopify/d/home.htmld and https://medium.com/analytics-vidhya/deploy-a-web-api-with-python-flask-and-mongodb-on-heroku-in-10-mins-71c4571c505d
-
-ca = certifi.where()
-app = Flask(__name__)
-"""
-
-
 uri = os.environ.get('MONGODB_URI')
 client = MongoClient(uri)
 db = client.get_database('CharityTracker')
 
-
-
-#ca = certifi.where()
-#host = os.environ.get('MONGODB_URI') 
-
-
-
-
-#create instance of the flask server (Server:Flask)
-#as the root directory within 'main.py'
 app = Flask(__name__)
 
-# create some routes!
-# Anytime I want to interact with the SERVER FLASK I have to reference the "app" variable
-# the @ is a python DECORATOR
-# The / is the DEFAULT slash
-
 """ 
-/users	        GET	        index	    See all users
-/users/new	    GET	        new	      See new user form
-/users	        POST	      create	  Create a new user
-/users/:id	    GET	        show	    See one user
-/users/:id/edit	GET	        edit	    See an edit user form
-/users/:id	    PATCH/PUT	  update	  Update a user
-/users/:id	    DELETE	    destroy	  Delete a user
-
 /donations	        GET	        index	    See all donations      <----- MAYBE
 /donations	        POST	      create	  Create a new donation
 /donations/:id/edit	GET	        edit	    See an edit donation form
@@ -74,12 +36,6 @@ MAYBE ITEMS TO DO:
 /charities/:id	    PATCH/PUT	  update	  Update a charity
 /charities/:id	    DELETE	    destroy	  Delete a charity
  """
-
-
-# users = [
-#     { 'name': 'Jim Corning', 'username': 'jCorn' },
-#     { 'name': 'George Lucas', 'username': 'gLucas' }
-# ]
 
 # ONLY ALLOW THE WEBSITE TO DISPLAY (Drop Down) A CHARITY LISTED below (to start)
 # TO DO - Allow user to enter a new charity, and once listed, allow to be chosen from drop down
@@ -153,6 +109,23 @@ def donation_new():
   # Hidden Form element to add the donation to the user
     return render_template('donations_new.html')
 
+"""
+@app.route('users/donations', method=['POST'])
+def donations_new():
+  donation = {
+      'user_id': request.form.get('user_id'),
+    # Name of the charity
+      'charityName': request.form.get('charity'),
+    # Calendar Picker for Date of Donation
+      'date': request.form.get('date'),
+    # Amount of Donation
+      'amount': request.form.get('amount'),
+    }
+  db.donations.insert_one(donation)
+  return redirect(url_for('users_show', user_id=user_id))
+  # Hidden Form element to add the donation to the user
+"""
+
 @app.route('/donations', methods=['POST'])
 def donation_submit():
     db.donation = {
@@ -173,43 +146,15 @@ def donations_del(donations_id):
     db.donations.delete_one({'_id': ObjectId(donations_id)})
     return redirect(url_for('donations_index'))
 
-
 #DONATION ABOVE: --------------------------------------------------------------    
 
-
-"""
-#CHARITY BELOW: ---------------------------------------------------------------
+#CHARITIES BELOW: --------------------------------------------------------------    
 
 @app.route('/charities/new')
 def charities_new():
     return render_template('charities_new.html')
 
-@app.route('/charities', methods=['POST'])
-def charities_submit():
-    charity = {
-      # Create a new Charity Organization
-        'name': request.form.get('charity'),
-      # What is the Charities Impact Score
-        'impact': request.form.get('impact'),
-      # Small description/information about Charity
-        'info': request.form.get('info'),
-      }
-    charities.insert_one(charity)
-    return redirect(url_for('charities_index'))
-
-@app.route('/charities/<charities_id>/remove', methods=['POST'])
-def charities_del(charities_id):
-    charities.delete_one({'_id': ObjectId(charities_id)})
-    return redirect(url_for('charities_index'))
-
-#CHARITY ABOVE: --------------------------------------------------------------
-
-
-
-"""
-
-
-
+#CHARITIES ABOVE: --------------------------------------------------------------    
 
 
 
